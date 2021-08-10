e2cbd13ca529c349f7e2ee4cf8f4423ed0f37794
akwasijfrimpong <52512868+akwasijfrimpong@users.noreply.github.com>
2021-08-10 16:32:15 +1200

    Add files via upload

---
+++ bmi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+
+
+namespace BMI
+{
+
+    class Program
+    {
+        public static string overweight(float bmi)
+        {
+            if(bmi < 18.5) {
+                return "You are under weight";
+                    }
+            else if(bmi >= 18.5 && bmi < 24.9){
+                return "You are in a healthy weight range";
+
+            }
+            else if(bmi >= 25 && bmi < 29.9)
+            {
+                return "You are overweight";
+            }
+            else
+            {
+                return "I'm sorry but you are obese";
+            }
+            
+            }
+            public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Height:");
+            float height = Convert.ToSingle(Console.ReadLine());
+            float bmi = weight / (height * height);
+      
+            string str = overweight(bmi);
+            Console.WriteLine(str + ". Your bmi is "+ Convert.ToInt32(bmi));
+           
+
+        }
+    }
+ 
+}
+
